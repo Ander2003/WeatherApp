ef5dbb87de6146e76f41c3c078ca1d6a122efd6f
unaxG <122366560+unaxG@users.noreply.github.com>
2023-10-18 08:28:38 +0200

    Agregar archivos de proyecto.

---
+++ Config.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherApp
+{
+    public class Config
+    {
+        //pongo mi clave de geoapify
+
+        public static string GeoAplifyKey = "238b7769b81b4babb9d5abad4162d5a6";
+    }
+}
---
+++ EndPoints.cs
@@ -0,0 +1,17 @@
+﻿namespace WeatherApp
+{
+    public class EndPoints
+    {
+        public static string GetGeoAplifyEndPoint(string cityName)
+        {
+           //return $"https://api.geoapify.com/v1/geocode/search?text=Bilbao&format=json&apiKey=ed907bcaa07a43a09bf900649ee59313";
+           return $"https://api.geoapify.com/v1/geocode/search?text={cityName}&format=json&apiKey={Config.GeoAplifyKey}";
+        }
+
+        public static string GetWeayherEndPoint(float lat, float lon)
+        {
+            var url = $"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current=temperature_2m,relativehumidity_2m&hourly=temperature_2m&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=Europe%2FLondon";
+            return url;
+        }
+    }
+}
